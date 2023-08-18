960bc5028f06473cc8315f58b7d4b2436f2cbaf2
NITESH <nitesh.k@sarv.com>
2023-08-18 19:50:19 +0530

    create react app

---
+++ chat-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>
+      "Embrace the journey of development, for in the realm of code, every line written is a step closer to innovation." 🚀👩‍💻👨‍💻
+      </h1>
+    </>
+  );
+}
+
+export default App;
