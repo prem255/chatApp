6c13055e7f1622bf962f014b061352a496d8686b
NITESH <nitesh.k@sarv.com>
2023-08-19 15:28:51 +0530

    add dashboard

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Login from "./components/Login"
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <Login />
+    <>
+    {/* <Login /> */}
+      <Dashboard />
+    </>
   );
 }
 
